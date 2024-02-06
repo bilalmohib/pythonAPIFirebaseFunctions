34f4abb0dc80b645e0beabe058d73c33df8c6aa4
Ammar mohib <66912469+AmmarMohib@users.noreply.github.com>
2024-02-06 23:08:03 +0500

    new code

--- server/src/app.py
+++ server/src/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
+port = int(os.environ.get("PORT", 8080))
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
+    # return "Hello World"
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0', port=port)
