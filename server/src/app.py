91d8ea7505b3de12c1f1567c13b2300fb11f528e
Ammar mohib <66912469+AmmarMohib@users.noreply.github.com>
2024-02-06 23:10:39 +0500

    new cod

--- server/src/app.py
+++ server/src/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
-from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
 port = int(os.environ.get("PORT", 8080))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = {
+        'title': 'sample data',
+        'content': 'This is some placeholder content for the sample data.'
+    }
+    return data
+    # return render_template('index.html')
     # return "Hello World"
+@app.route('/second_page')
+def second_page():
+    # Placeholder data
+    data = {
+        'title': 'Second Page',
+        'content': 'This is some placeholder content for the second page.'
+    }
+    return render_template('second_page.html', data=data)
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=port)
